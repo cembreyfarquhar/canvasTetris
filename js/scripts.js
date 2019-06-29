983d6ee2236cb602189137af05bbc3a53b62ca0f
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-06-29 11:23:55 -0500

    Creates MD for a textual description of needed functions

--- js/scripts.js
+++ js/scripts.js
@@ -2,34 +2,51 @@ const canvas = document.querySelector('#canvas')
 
 // Set up the board
 const width = (canvas.width = 528)
-const height = (canvas.height = window.screen.availHeight - 150)
-
 const bsl = 44
+
+const height = (canvas.height = window.screen.availHeight - bsl * 3)
+
 const halfL = bsl / 2
+const blocksHigh = 20
+const blocksWide = 12
 // margin 4 on each side
 // 12 blocks wide, 20 blocks high
 
 const ctx = document.getElementById('canvas').getContext('2d')
-// let x = width / 4
-// let y = 100
+// // make some basic shapes
+// function drawTri(x, y) {
+//     ctx.beginPath()
+//     ctx.moveTo(x - halfL * 3, y)
+//     ctx.lineTo(x + halfL * 3, y)
+//     ctx.lineTo(x + halfL * 3, y - bsl)
+//     ctx.lineTo(x + halfL * 1, y - bsl)
+//     ctx.lineTo(x + halfL * 1, y - bsl * 2)
+//     ctx.lineTo(x + halfL * 1, y)
+//     ctx.lineTo(x - halfL, y)
+//     ctx.lineTo(x - halfL, y - 2 * bsl)
+//     ctx.lineTo(x + halfL, y - 2 * bsl)
+//     ctx.lineTo(x + halfL, y)
+//     ctx.lineTo(x - halfL * 3, y)
+//     ctx.lineTo(x - halfL * 3, y - bsl)
+//     ctx.lineTo(x + halfL * 3, y - bsl)
+//     ctx.stroke()
+// }
 
-drawTri(bsl + halfL, bsl * 2)
+// function play() {
+//     let x = bsl + halfL
+//     let y = bsl * 2
 
-// make some basic shapes
-function drawTri(x, y) {
-    ctx.beginPath()
-    ctx.moveTo(x - halfL * 3, y)
-    ctx.lineTo(x + halfL * 3, y)
-    ctx.lineTo(x + halfL * 3, y - bsl)
-    ctx.lineTo(x + halfL * 1, y - bsl)
-    ctx.lineTo(x + halfL * 1, y - bsl * 2)
-    ctx.lineTo(x + halfL * 1, y)
-    ctx.lineTo(x - halfL, y)
-    ctx.lineTo(x - halfL, y - 2 * bsl)
-    ctx.lineTo(x + halfL, y - 2 * bsl)
-    ctx.lineTo(x + halfL, y)
-    ctx.lineTo(x - halfL * 3, y)
-    ctx.lineTo(x - halfL * 3, y - bsl)
-    ctx.lineTo(x + halfL * 3, y - bsl)
-    ctx.stroke()
-}
+//     setInterval(() => {
+//         if (y < bsl * (blocksHigh + 2)) {
+//             clearPrev(y - bsl * 1)
+//         }
+//         drawTri(x, y)
+//         y += bsl
+//     }, 100)
+// }
+
+// function clearPrev(distanceDown) {
+//     ctx.clearRect(0, 0, canvas.width, distanceDown)
+// }
+
+play()
---
+++ text_guide.md
@@ -0,0 +1,13 @@
+Every 'second', the following needs to happen:
+    - if completed line:
+        - clear line+ all lines above it, move all shapes down one block space
+    
+    - if current shape is not on top of another shape or the ground:
+        - erase current shape, move shape down one block space
+    
+
+Store a list of:
+    - each shape
+        - color
+        - position
+        - shape
