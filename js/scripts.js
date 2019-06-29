7269685f9a609e14ed6909576c9418159f88adc1
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-06-29 13:28:23 -0500

    draws a grid over the matrix

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ const blocksHigh = 20
 const blocksWide = 12
 
 // margin 4 on each side
-// 12 blocks wide, 20 blocks high
+// 12 blocks wide, 21 blocks high
 
 // Set up the canvas
 const mapWidth = 528
@@ -17,109 +17,181 @@ canvas.height = mapHeight
 
 const ctx = document.getElementById('canvas').getContext('2d')
 
-// class for every shape
-class Shape {
-    constructor() {
-        this.edges = []
-    }
-
-    drawLine = (x, y) => {
-        ctx.lineTo(x, y)
-    }
-
-    addEdge = (start, end) => {
-        this.edges.push([start, end])
-    }
-
-    clearEdges = () => {
-        this.edges = []
+class Block {
+    constructor(x, y) {
+        this.edges = [
+            [x, y],
+            [x + sideLength, y],
+            [x + sideLength, y - sideLength],
+            [x, y - sideLength]
+        ]
     }
 }
 
-// class for Line shape
-class LineShape extends Shape {
-    constructor(Shape) {
-        super(Shape)
-        this.xPosition = this.getRandomX()
-    }
+// draw a grid over the matrix
+ctx.beginPath()
+ctx.moveTo(sideLength, 0)
+ctx.lineTo(sideLength, sideLength * 21)
+ctx.moveTo(sideLength * 2, 0)
+ctx.lineTo(sideLength * 2, sideLength * 21)
+ctx.moveTo(sideLength * 3, 0)
+ctx.lineTo(sideLength * 3, sideLength * 21)
+ctx.moveTo(sideLength * 4, 0)
+ctx.lineTo(sideLength * 4, sideLength * 21)
+ctx.moveTo(sideLength * 5, 0)
+ctx.lineTo(sideLength * 5, sideLength * 21)
+ctx.moveTo(sideLength * 6, 0)
+ctx.lineTo(sideLength * 6, sideLength * 21)
+ctx.moveTo(sideLength * 7, 0)
+ctx.lineTo(sideLength * 7, sideLength * 21)
+ctx.moveTo(sideLength * 8, 0)
+ctx.lineTo(sideLength * 8, sideLength * 21)
+ctx.moveTo(sideLength * 9, 0)
+ctx.lineTo(sideLength * 9, sideLength * 21)
+ctx.moveTo(sideLength * 10, 0)
+ctx.lineTo(sideLength * 10, sideLength * 21)
+ctx.moveTo(sideLength * 11, 0)
+ctx.lineTo(sideLength * 11, sideLength * 21)
 
-    getRandomX = () => {
-        const rand = Math.floor(Math.random() * (mapWidth - 3 * sideLength))
-        const remainder = rand % sideLength
-        return rand - remainder
-    }
+ctx.moveTo(0, sideLength)
+ctx.lineTo(sideLength * 12, sideLength)
+ctx.moveTo(0, sideLength * 2)
+ctx.lineTo(sideLength * 12, sideLength * 2)
+ctx.moveTo(0, sideLength * 2)
+ctx.lineTo(sideLength * 12, sideLength * 2)
+ctx.moveTo(0, sideLength * 3)
+ctx.lineTo(sideLength * 12, sideLength * 3)
+ctx.moveTo(0, sideLength * 4)
+ctx.lineTo(sideLength * 12, sideLength * 4)
+ctx.moveTo(0, sideLength * 5)
+ctx.lineTo(sideLength * 12, sideLength * 5)
+ctx.moveTo(0, sideLength * 6)
+ctx.lineTo(sideLength * 12, sideLength * 6)
+ctx.moveTo(0, sideLength * 7)
+ctx.lineTo(sideLength * 12, sideLength)
+ctx.moveTo(0, sideLength * 8)
+ctx.lineTo(sideLength * 12, sideLength)
+ctx.moveTo(0, sideLength * 9)
+ctx.lineTo(sideLength * 12, sideLength)
+ctx.moveTo(0, sideLength * 10)
+ctx.lineTo(sideLength * 12, sideLength)
+ctx.moveTo(0, sideLength * 11)
+ctx.lineTo(sideLength * 12, sideLength)
+ctx.moveTo(0, sideLength * 12)
+ctx.lineTo(sideLength * 12, sideLength)
+ctx.moveTo(0, sideLength * 13)
+ctx.lineTo(sideLength * 12, sideLength)
+ctx.moveTo(0, sideLength * 14)
+ctx.lineTo(sideLength * 12, sideLength)
+ctx.moveTo(0, sideLength * 15)
+ctx.lineTo(sideLength * 12, sideLength)
 
-    draw = y => {
-        ctx.beginPath()
-        ctx.moveTo(this.xPosition, y)
-        this.drawLine(this.xPosition + 4 * sideLength, y)
-        this.addEdge([this.xPosition, y], [this.xPosition + 4 * sideLength, y])
+ctx.strokeStyle = 'blue'
+ctx.stroke()
 
-        this.drawLine(this.xPosition + 4 * sideLength, y - sideLength)
-        this.addEdge(this.getLastPoint(), [
-            this.xPosition + 4 * sideLength,
-            y - sideLength
-        ])
+// // class for every shape
+// class Shape {
+//     constructor() {
+//         this.edges = []
+//     }
 
-        this.drawLine(this.xPosition, y - sideLength)
-        this.drawLine(this.xPosition, y)
-        this.drawLine(this.xPosition + 1 * sideLength, y)
-        this.drawLine(this.xPosition + 1 * sideLength, y - sideLength)
-        this.drawLine(this.xPosition + 2 * sideLength, y - sideLength)
-        this.drawLine(this.xPosition + 2 * sideLength, y)
-        this.drawLine(this.xPosition + 3 * sideLength, y)
-        this.drawLine(this.xPosition + 3 * sideLength, y - sideLength)
+//     drawLine = (x, y) => {
+//         ctx.lineTo(x, y)
+//     }
 
-        ctx.stroke()
-    }
+//     addEdge = (start, end) => {
+//         this.edges.push([start, end])
+//     }
 
-    getLastPoint = () => this.edges[this.edges.length - 1][1]
-
-    getEdges = () => {
-        return this.edges
-    }
-}
-
-const Line = new LineShape()
-Line.clearEdges()
-
-Line.draw(sideLength)
-
-// // make some basic shapes
-// function drawTri(x, y) {
-//     ctx.beginPath()
-//     ctx.moveTo(x - halfL * 3, y)
-//     ctx.lineTo(x + halfL * 3, y)
-//     ctx.lineTo(x + halfL * 3, y - bsl)
-//     ctx.lineTo(x + halfL * 1, y - bsl)
-//     ctx.lineTo(x + halfL * 1, y - bsl * 2)
-//     ctx.lineTo(x + halfL * 1, y)
-//     ctx.lineTo(x - halfL, y)
-//     ctx.lineTo(x - halfL, y - 2 * bsl)
-//     ctx.lineTo(x + halfL, y - 2 * bsl)
-//     ctx.lineTo(x + halfL, y)
-//     ctx.lineTo(x - halfL * 3, y)
-//     ctx.lineTo(x - halfL * 3, y - bsl)
-//     ctx.lineTo(x + halfL * 3, y - bsl)
-//     ctx.stroke()
+//     clearEdges = () => {
+//         this.edges = []
+//     }
 // }
 
-// function play() {
-//     let x = bsl + halfL
-//     let y = bsl * 2
+// // class for Line shape
+// class LineShape extends Shape {
+//     constructor(Shape) {
+//         super(Shape)
+//         this.xPosition = this.getRandomX()
+//     }
 
-//     setInterval(() => {
-//         if (y < bsl * (blocksHigh + 2)) {
-//             clearPrev(y - bsl * 1)
-//         }
-//         drawTri(x, y)
-//         y += bsl
-//     }, 100)
+//     getRandomX = () => {
+//         const rand = Math.floor(Math.random() * (mapWidth - 3 * sideLength))
+//         const remainder = rand % sideLength
+//         return rand - remainder
+//     }
+
+//     draw = y => {
+//         ctx.beginPath()
+//         ctx.moveTo(this.xPosition, y)
+//         this.drawLine(this.xPosition + 4 * sideLength, y)
+//         this.addEdge([this.xPosition, y], [this.xPosition + 4 * sideLength, y])
+
+//         this.drawLine(this.xPosition + 4 * sideLength, y - sideLength)
+//         this.addEdge(this.getLastPoint(), [
+//             this.xPosition + 4 * sideLength,
+//             y - sideLength
+//         ])
+
+//         this.drawLine(this.xPosition, y - sideLength)
+//         this.drawLine(this.xPosition, y)
+//         this.drawLine(this.xPosition + 1 * sideLength, y)
+//         this.drawLine(this.xPosition + 1 * sideLength, y - sideLength)
+//         this.drawLine(this.xPosition + 2 * sideLength, y - sideLength)
+//         this.drawLine(this.xPosition + 2 * sideLength, y)
+//         this.drawLine(this.xPosition + 3 * sideLength, y)
+//         this.drawLine(this.xPosition + 3 * sideLength, y - sideLength)
+
+//         ctx.stroke()
+//     }
+
+//     getLastPoint = () => this.edges[this.edges.length - 1][1]
+
+//     getEdges = () => {
+//         return this.edges
+//     }
 // }
 
-// function clearPrev(distanceDown) {
-//     ctx.clearRect(0, 0, canvas.width, distanceDown)
-// }
+// const Line = new LineShape()
+// Line.clearEdges()
+
+// Line.draw(sideLength)
+
+// // // make some basic shapes
+// // function drawTri(x, y) {
+// //     ctx.beginPath()
+// //     ctx.moveTo(x - halfL * 3, y)
+// //     ctx.lineTo(x + halfL * 3, y)
+// //     ctx.lineTo(x + halfL * 3, y - bsl)
+// //     ctx.lineTo(x + halfL * 1, y - bsl)
+// //     ctx.lineTo(x + halfL * 1, y - bsl * 2)
+// //     ctx.lineTo(x + halfL * 1, y)
+// //     ctx.lineTo(x - halfL, y)
+// //     ctx.lineTo(x - halfL, y - 2 * bsl)
+// //     ctx.lineTo(x + halfL, y - 2 * bsl)
+// //     ctx.lineTo(x + halfL, y)
+// //     ctx.lineTo(x - halfL * 3, y)
+// //     ctx.lineTo(x - halfL * 3, y - bsl)
+// //     ctx.lineTo(x + halfL * 3, y - bsl)
+// //     ctx.stroke()
+// // }
+
+// // function play() {
+// //     let x = bsl + halfL
+// //     let y = bsl * 2
+
+// //     setInterval(() => {
+// //         if (y < bsl * (blocksHigh + 2)) {
+// //             clearPrev(y - bsl * 1)
+// //         }
+// //         drawTri(x, y)
+// //         y += bsl
+// //     }, 100)
+// // }
+
+// // function clearPrev(distanceDown) {
+// //     ctx.clearRect(0, 0, canvas.width, distanceDown)
+// // }
 
 play()
 
